c603bb748f202a4453c17a2379ffbd319260a80c
Abed Nganga <ngangaabed@gmail.com>
2023-08-27 11:56:37 +0300

    Added Restaurant class

---
+++ lib/restaurant.py
@@ -0,0 +1,14 @@
+class Restaurant:
+    def __init__ (self, name):
+        self.name = name
+        self.reviews = []
+        
+    def name (self):
+        return self.name
+    
+    def reviews (self):
+        return self.reviews
+    
+    def customers (self):
+        pass
+        
